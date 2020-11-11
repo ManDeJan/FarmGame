9d8b9c65fecfa10f64e3dc2e01104def55c636ca
Jan Halsema <janhlsm@gmail.com>
2020-11-11 02:20:01 +0100

    Update server code

--- Server/highscore.py
+++ Server/highscore.py
@@ -1,28 +1,54 @@
+#!/bin/python
 import sqlite3
 import base64
 from itertools import cycle
-from flask import Flask, request
+from flask import Flask, request, g, escape
 from datetime import datetime
 app = Flask(__name__)
-conn = sqlite3.connect('highscores.sqlite')
 
+DATABASE = 'highscores.sqlite'
 
-@app.route('/')
-def home():
-    return '''
-    <html>
-    <head>
-    </head>
-    <body>
-        <a href="/highscores">Highscores</a>
-    </body>
-    </html>
-    '''
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 def show_highscores():
+    conn = get_db()
     return_val = '''<html>
     <head>
+    
+    <style>
+        html {
+            font-family: 'Open Sans', sans-serif;
+            margin: 0;
+        }
+
+        body {
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            margin: 0;
+            background: #232323;
+            color: #fdfff1;
+            font-size: 4vw;
+            font-weight: 500;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
     </head>
     <body>
     <table>
@@ -34,10 +60,10 @@ def show_highscores():
     </tr>
     '''
     cursor = conn.execute(
-        '''SELECT Username, Score FROM highscores ORDER BY highscores.Score LIMIT 50''')
-    print(cursor)
+        '''SELECT Username, Score FROM highscores ORDER BY highscores.Score DESC LIMIT 50''')
+    # print(cursor)
     for row in cursor:
-        print(row)
+        # print(row)
         return_val += f'''
         <tr>
             <td>{row[0]}</td>
@@ -57,20 +83,32 @@ def decrypt_highscore(encrypted_text):
 
 
 def post_highscore():
+    print('werkpls')
+    conn = get_db()
     name, highscore,validation = decrypt_highscore(request.form['super_secret'])
-    print(name, highscore, validation)
+    # print(name, highscore, validation)
     if validation != 'THISMUSTALWAYSBETHESAME':
         return ("NO", 404)
+    name = escape(name)
+    highscore = escape(highscore)
+    if len(name) > 20:
+        name = name[:20]
+    if not name:
+        name = 'Anonymous'
     conn.execute('''INSERT INTO highscores(Timestamp, Username, Score) VALUES (?,?,?)''', [
                  int(datetime.now().timestamp()), name, highscore])
     conn.commit()
+    conn.close()
     return "OK"
 
 
 @app.route('/highscores', methods=['GET', 'POST'])
 def highscores():
-    print("EYY")
+    print('biepboop all is cool')
     if request.method == 'GET':
         return show_highscores()
     else:
         return post_highscore()
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
